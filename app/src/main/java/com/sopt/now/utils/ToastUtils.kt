24453a4d2416b72a225fa7b26c25b6a5bd43dfdc
SYAAINN <alswo08319@gmail.com>
2024-08-01 13:16:22 +0900

    [FEAT/#1] Toast utils 생성

---
+++ app/src/main/java/com/sopt/now/utils/ToastUtils.kt
@@ -0,0 +1,12 @@
+package com.sopt.now.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context,message, Toast.LENGTH_SHORT).show()
+}
+
+fun showLongToast(context: Context, message: String) {
+    Toast.makeText(context,message, Toast.LENGTH_LONG).show()
+}
