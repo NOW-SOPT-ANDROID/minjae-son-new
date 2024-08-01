73ee837f4a54c0174a973d9f22f7eb8244444229
SYAAINN <alswo08319@gmail.com>
2024-08-01 13:32:18 +0900

    [FEAT/#1] MainActivity 기능구현

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -2,13 +2,25 @@ package com.sopt.now
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sopt.now.data.User
 import com.sopt.now.databinding.ActivityMainBinding
+import com.sopt.now.utils.getParcelable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var user: User? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        user = intent.getParcelable("user", User::class.java)
+
+        with(binding) {
+            tvMyPageNickname.text = user?.nickname
+            tvMyPageIdContent.text = user?.id
+            tvMyPagePwContent.text = user?.password
+            tvMyPagePhoneNumberContent.text = user?.phoneNumber
+        }
     }
 }
