17096106d6f1335c18a19eae735da909126a280c
SYAAINN <alswo08319@gmail.com>
2024-08-06 19:45:21 +0900

    [CHORE/#2] SignUpValidation object화

--- app/src/main/java/com/sopt/now/compose/presentation/ui/auth/screen/SignUpValidation.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/ui/auth/screen/SignUpValidation.kt
@@ -2,38 +2,37 @@ package com.sopt.now.compose.presentation.ui.auth.screen
 
 import com.sopt.now.compose.data.User
 
-class SignUpValidation {
-    companion object {
-        private const val ID_MIN_LENGTH = 6
-        private const val ID_MAX_LENGTH = 10
-        private const val PW_MIN_LENGTH = 8
-        private const val PW_MAX_LENGTH = 12
+object SignUpValidation {
 
-        private fun isIdValid(inputId: String): Boolean {
-            val regex = "^[a-zA-Z0-9]{$ID_MIN_LENGTH,$ID_MAX_LENGTH}$".toRegex()
-            return regex.matches(inputId)
-        }
+    private const val ID_MIN_LENGTH = 6
+    private const val ID_MAX_LENGTH = 10
+    private const val PW_MIN_LENGTH = 8
+    private const val PW_MAX_LENGTH = 12
 
-        private fun isPasswordValid(inputPassword: String): Boolean {
-            val regex = "^[a-zA-Z0-9]{$PW_MIN_LENGTH,$PW_MAX_LENGTH}$".toRegex()
-            return regex.matches(inputPassword)
-        }
+    private fun isIdValid(inputId: String): Boolean {
+        val regex = "^[a-zA-Z0-9]{$ID_MIN_LENGTH,$ID_MAX_LENGTH}$".toRegex()
+        return regex.matches(inputId)
+    }
 
-        private fun isNicknameValid(inputNickname: String): Boolean {
-            val regex = "^\\S+$".toRegex()
-            return regex.matches(inputNickname)
-        }
+    private fun isPasswordValid(inputPassword: String): Boolean {
+        val regex = "^[a-zA-Z0-9]{$PW_MIN_LENGTH,$PW_MAX_LENGTH}$".toRegex()
+        return regex.matches(inputPassword)
+    }
 
-        private fun isPhoneNumberValid(inputPhoneNumber: String): Boolean {
-            val regex = "^010-\\d{4}-\\d{4}$".toRegex()
-            return regex.matches(inputPhoneNumber)
-        }
+    private fun isNicknameValid(inputNickname: String): Boolean {
+        val regex = "^\\S+$".toRegex()
+        return regex.matches(inputNickname)
+    }
 
-        fun isSignUpValid(user: User): Boolean {
-            return isIdValid(user.id) &&
-                    isPasswordValid(user.password) &&
-                    isNicknameValid(user.nickname) &&
-                    isPhoneNumberValid(user.phoneNumber)
-        }
+    private fun isPhoneNumberValid(inputPhoneNumber: String): Boolean {
+        val regex = "^010-\\d{4}-\\d{4}$".toRegex()
+        return regex.matches(inputPhoneNumber)
+    }
+
+    fun isSignUpValid(user: User): Boolean {
+        return isIdValid(user.id) &&
+                isPasswordValid(user.password) &&
+                isNicknameValid(user.nickname) &&
+                isPhoneNumberValid(user.phoneNumber)
     }
 }
