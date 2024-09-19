4d867c55fb043c1bf884f11e5b5eb03bc41571b2
SYAAINN <alswo08319@gmail.com>
2024-09-19 20:14:51 +0900

    [Feat/#5] SignUpActivity - ViewModel 분리

--- app/src/main/java/com/sopt/now/presentation/ui/auth/signup/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/presentation/ui/auth/signup/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.sopt.now.presentation.ui.auth.signup
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.sopt.now.R
 import com.sopt.now.data.User
 import com.sopt.now.databinding.ActivitySignupBinding
@@ -12,12 +13,26 @@ import com.sopt.now.presentation.utils.showToast
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
+    private lateinit var viewModel: SignUpViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
 
         onSignUpClicked()
+    }
+
+    private fun setupObservers() {
+        viewModel.signUpState.observe(this) { isSuccess ->
+            if (isSuccess) {
+                showToast(this@SignUpActivity, getString(R.string.signup_signup_success))
+                navigateToSignIn(viewModel.user.value)
+            } else {
+                showToast(this@SignUpActivity, getString(R.string.signup_signup_failure))
+            }
+        }
     }
 
     private fun onSignUpClicked() {
@@ -29,19 +44,14 @@ class SignUpActivity : AppCompatActivity() {
                     etSignUpNickname.text.toString(),
                     etSignUpPhoneNumber.text.toString()
                 )
-
-                if (SignUpValidation.isSignUpValid(user)
-                ) {
-                    showToast(this@SignUpActivity, getString(R.string.signup_signup_success))
-                    navigateToSignIn(user)
-                } else {
-                    showToast(this@SignUpActivity, getString(R.string.signup_signup_failure))
-                }
+                viewModel.setUser(user)
             }
+            viewModel.validateSignUp()
+            setupObservers()
         }
     }
 
-    private fun navigateToSignIn(user: User) {
+    private fun navigateToSignIn(user: User?) {
         val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
         intent.putExtra(KeyStorage.USER_INFO, user)
         startActivity(intent)
